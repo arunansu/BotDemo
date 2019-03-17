c275fddf45f30aa39acfb9da67864f1205e76fc6
Arunansu Pattanayak <arpattan@microsoft.com>
2019-03-17 16:19:30 -0400

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=arun-webapp-bot-Web-Deploy.pubxml -p:Password=9mfGFy5yDa4RnT1t9vvhhGdakD3mpxBc7jhd63GNFAuFZBiKn6f4738CpJMs
+
